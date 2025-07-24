779541ec85100e3ab9a61a828ac12112f99207dc
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-07-24 15:53:57 -0400

    feat: replace alert with dialog for deletion in item menu

--- view/components/shared/item-menu.tsx
+++ view/components/shared/item-menu.tsx
@@ -2,13 +2,22 @@
 
 import { Button } from '@/components/ui/button';
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { cn } from '@/lib/shared.utils';
-import { ReactNode } from 'react';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
+import { ReactNode, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { LuPencil, LuTrash } from 'react-icons/lu';
 import { MdMoreHoriz } from 'react-icons/md';
@@ -45,6 +54,8 @@ const ItemMenu = ({
   updateBtnLabel,
   variant = 'ghost',
 }: Props) => {
+  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+
   const { t } = useTranslation();
   const navigate = useNavigate();
 
@@ -70,10 +81,8 @@ const ItemMenu = ({
       return;
     }
     deleteItem();
+    setIsDeleteDialogOpen(false);
   };
-
-  const handleDeleteWithPrompt = () =>
-    window.confirm(deletePrompt) && handleDelete();
 
   return (
     <DropdownMenu>
@@ -108,7 +117,7 @@ const ItemMenu = ({
 
         {showDeleteButton && (
           <DropdownMenuItem
-            onClick={handleDeleteWithPrompt}
+            onClick={() => setIsDeleteDialogOpen(true)}
             variant="destructive"
           >
             <LuTrash className="h-4 w-4" />
@@ -118,6 +127,30 @@ const ItemMenu = ({
 
         {!prependChildren && children}
       </DropdownMenuContent>
+
+      <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
+        <DialogContent>
+          <DialogHeader className="mt-9">
+            <DialogTitle className="text-md font-normal">
+              {deletePrompt}
+            </DialogTitle>
+            <VisuallyHidden>
+              <DialogDescription>{deletePrompt}</DialogDescription>
+            </VisuallyHidden>
+          </DialogHeader>
+          <DialogFooter className="flex flex-row gap-2 self-center">
+            <Button
+              variant="outline"
+              onClick={() => setIsDeleteDialogOpen(false)}
+            >
+              {t('actions.cancel')}
+            </Button>
+            <Button variant="destructive" onClick={handleDelete}>
+              {deleteBtnLabel || t('actions.delete')}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </DropdownMenu>
   );
 };
