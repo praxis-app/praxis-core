68d0576a5c9ae729f668e98506750d9507b4e722
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-08-04 00:00:42 -0400

    feat: route to home if fetch channel fails

--- view/pages/channels/channel-page.tsx
+++ view/pages/channels/channel-page.tsx
@@ -1,14 +1,25 @@
 import { api } from '@/client/api-client';
 import { ChannelView } from '@/components/channels/channel-view';
+import { NavigationPaths } from '@/constants/shared.constants';
 import { useQuery } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export const ChannelPage = () => {
   const { channelId } = useParams();
+  const navigate = useNavigate();
 
   const { data: channelData } = useQuery({
     queryKey: ['channels', channelId],
-    queryFn: () => api.getChannel(channelId!),
+    queryFn: async () => {
+      try {
+        const result = await api.getChannel(channelId!);
+        return result;
+      } catch (error) {
+        await navigate(NavigationPaths.Home);
+        console.error(error);
+        return null;
+      }
+    },
     enabled: !!channelId,
   });
 
