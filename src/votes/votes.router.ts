6a646b47b0efd3223fb6e6e280b05aff5bc99079
forrestwilkins <ethanforrestwilkins@gmail.com>
2025-08-09 14:30:58 -0400

    refactor: add todo comment

--- src/votes/votes.router.ts
+++ src/votes/votes.router.ts
@@ -1,3 +1,5 @@
+// TODO: Guard routes with permission checks
+
 import express from 'express';
 import { authenticate } from '../auth/middleware/authenticate.middleware';
 import { createVote, deleteVote, updateVote } from './votes.controller';
